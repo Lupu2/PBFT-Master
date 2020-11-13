bc0af702aa80ab7c3d18608daf341542375e03a5
Stidsborg <stidsborg@cleipnir.net>
2020-11-13 08:30:35 +0100

    Removed Network Communication Project

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,16 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Cleipnir.ExecutionEngine;
-using Cleipnir.ExecutionEngine.Providers;
-using Cleipnir.NetworkCommunication;
-using Cleipnir.ObjectDB;
-using Cleipnir.ObjectDB.Persistency;
-using Cleipnir.ObjectDB.Persistency.Deserialization;
-using Cleipnir.ObjectDB.Persistency.Serialization;
-using Cleipnir.ObjectDB.Persistency.Serialization.Serializers;
-using Cleipnir.ObjectDB.TaskAndAwaitable.StateMachine;
-using Cleipnir.StorageEngine.InMemory;
 
 namespace Playground
 {
@@ -18,7 +6,7 @@ namespace Playground
     {
         static void Main(string[] args)
         {
-            TravelAgent.P.Do();
+            SqlExample.P.Do();
             
             Console.WriteLine("PRESS ENTER TO EXIT");
             Console.ReadLine();
