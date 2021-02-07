109b4a9823ddc7b2b1c0bdefa1f5c5578fd1b695
Lupu2 <35259974+Lupu2@users.noreply.github.com>
2021-02-07 16:05:14 +0100

    porogram is not fun

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -7,8 +7,16 @@ namespace Playground
     {
         static void Main(string[] args)
         {
-            SingleMachinePingPong.P.StartNew(File);
+            //SingleMachinePingPong.P.StartNew(File);
+            //HeartbeatSender.P.Do();
+            //SayerExample.P.Do();
+            //TravelAgent.P.Do();
+            //SqlExample.P.Do();
+            //HelloMessage.P.DO();
             
+            //SimulateSendReceive.P.Do();
+            //MySqlStorageEngine.P.Do();
+            PoormansSchedulerOld.P.Do();
             Console.WriteLine("PRESS ENTER TO EXIT");
             Console.ReadLine();
         }
